07f7356ed2af6d5fbbffe143c2e3e2dd594d589c
Lelo88 <villalbaleandrogabriel@gmail.com>
2023-01-28 11:47:05 -0300

    intialize exercise

---
+++ pkg/store/interfaces.go
@@ -0,0 +1,18 @@
+package store
+
+import "github.com/Lelo88/EjercicioGoDb2/internal/domain"
+
+
+
+type StoreInterface interface {
+	// Read devuelve un producto por su id
+	Read(id int) (domain.Product, error)
+	// Create agrega un nuevo producto
+	Create(product domain.Product) error
+	// Update actualiza un producto
+	Update(product domain.Product) error
+	// Delete elimina un producto
+	Delete(id int) error
+	// Exists verifica si un producto existe
+	Exists(codeValue string) bool
+}
